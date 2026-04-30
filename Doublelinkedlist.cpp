36846452362f950552839d06a456234d737e4092
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-04-30 13:13:57 +0700

    menambahkan output

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -253,7 +253,10 @@ int main()
         default:
             cout << "Invalid option\n";
        }
-       
-    } 
-    
+       cout << "\nPress Enter to continue...";
+       cin.ignore();
+       cin.get();
+       cout << endl;
+       system("clear");
+    } while (choice != '6');
 }
