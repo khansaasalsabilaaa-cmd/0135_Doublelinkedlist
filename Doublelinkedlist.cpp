4e4472d850289f3e27a3dcedc8427b6f01f976f8
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-04-30 12:26:39 +0700

    menambahkan step 2

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -105,6 +105,17 @@ public:
             cout << "Record not found" << endl;
             return;
         }
-        
+
+        // Step 2: If node is at the beginning
+        if (current == START)
+        {
+            START = current->next; // Step 2a : START = START.next
+            if (START != NULL)
+                START->prev = NULL; // Step 2b : START.prev = NULL
+        }
+        else
+        {
+            // Step 3 : Link previous
+        }
     }
 }
